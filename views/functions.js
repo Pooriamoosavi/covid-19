50fda41497d86b6ac7fdd47ff0fad55824098625
pooriamoosavi <pouriaux@gmail.com>
2020-04-24 13:06:29 +0430

    3 pattern

--- views/functions.js
+++ views/functions.js
@@ -1,4 +1,5 @@
 exports = module.exports = {};
+
 function titleGen(province, title) {
   return province.length === 0
     ? title
@@ -6,4 +7,9 @@ function titleGen(province, title) {
         " | " +
         (province.length > 12 ? province.slice(0, 12) + "..." : province);
 }
+function numberWithCommas(x) {
+  if (x === null) return x;
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 exports.titleGen = titleGen;
+exports.numberWithCommas = numberWithCommas;
